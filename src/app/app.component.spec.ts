bee6f19cfa20421982803d562755eb9fe949c310
Andres Babativa <carlos3k11@gmail.com>
2025-04-16 22:19:05 -0500

    feat: update app component structure and styles for improved layout and responsiveness

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, proyectoPersonales-Angular-PokemosApi');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, proyectoPersonales-Angular-PokemosApi'
+    );
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
 })
 export class AppComponent {
   title = 'proyectoPersonales-Angular-PokemosApi';
