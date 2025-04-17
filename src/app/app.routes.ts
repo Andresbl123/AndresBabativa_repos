2fe870c61af5441e942c1ad21484cae931253db6
Andres Babativa <carlos3k11@gmail.com>
2025-04-16 22:20:04 -0500

    feat: update app configuration and routing with HTTP client and component routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CardDetailsComponent } from './pages/card-details/card-details.component';
+import { PokemonListComponent } from './pages/pokemon-list/pokemon-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PokemonListComponent },
+  { path: 'pokemon/:name', component: CardDetailsComponent },
+  { path: '**', redirectTo: '' },
+];
